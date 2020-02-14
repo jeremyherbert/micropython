999225c591789f6db441ea7d101e0e2aed3d960c
Jeremy Herbert <jeremy.006@gmail.com>
2020-02-14 21:03:10 +1000

    add implementation of CBOR encode/decode (ucbor)

---
+++ examples/natmod/ucbor/test.py
@@ -0,0 +1,31 @@
+import ucbor
+
+# generated using the python cbor2 library
+fixtures = {
+    "int": (b'\x01', 1),
+    "string": (b'cabc', "abc"),
+    "bytes": (b'Cabc', b"abc"),
+    "float64": (b'\xfb@\t\x1e\xb8Q\xeb\x85\x1f', 3.14),
+    "list": (b'\x83\x01\x02\x03', [1, 2, 3]),
+    "nested_list": (b'\x83\x83\x82\x01\x02\x82\x03\x04\x05\x06\x07', [[[1,2], [3, 4], 5], 6, 7]),
+    "dict": (b'\xa2dabcdcefgdhijkelmnop', {"hijk": "lmnop", "abcd": "efg"}),
+    "big_mix": (b'\xa2cabc\x82\x83\x82\x01\x02\x82\x03\x04\x05\xa2cdefCghicjlk\x07clmn\x08', {"abc": [[[1,2], [3, 4], 5], {"def": b"ghi", "jlk": 7}], "lmn": 8}),
+}
+
+for key, val in fixtures.items():
+    print("testing", key)
+    print(val[0], "==", val[1])
+    test = val[0]
+    result = ucbor.loads(test)
+    print(result)
+    assert result == val[1]
+    print("loads success")
+
+    # check if dumps then loads is identical
+    result = ucbor.loads(ucbor.dumps(result))
+    assert result == val[1]
+    print("dumps success\n\n")
+
+print("check tuple encodes to array")
+assert ucbor.dumps((1, 2, 3)) == b'\x83\x01\x02\x03'
+print("success")
